86d00f4ea7d2c30b667593cf0ddae072d82be6f2
Thomas Schmidt <ts@sinnwerkstatt.com>
2015-08-20 16:37:17 +0200

    Comment

--- static/js/script.js
+++ static/js/script.js
@@ -34,3 +34,14 @@ if ($('#newname-name').length) {
 	_paq.push(['setCustomVariable', 1, "Name", $('#newname-name').text(), "visit" ]);
 	_paq.push(['trackGoal', 1]);
 }
+
+
+/*
+Reset Database:
+DELETE FROM piwik_archive_blob_2015_08 WHERE idsite = 16;
+DELETE FROM piwik_archive_numeric_2015_08 WHERE idsite = 16;
+DELETE FROM piwik_log_visit WHERE idsite = 16;
+DELETE FROM piwik_log_link_visit_action WHERE idsite = 16;
+DELETE FROM piwik_log_conversion WHERE idsite = 16;
+DELETE FROM piwik_log_conversion_item WHERE idsite = 16;
+*/
